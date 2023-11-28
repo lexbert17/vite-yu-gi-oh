8efdc6f27cb9983889f3a9bbb4a27536e16fb5a1
lexbert17 <lexbertlazo@gmail.com>
2023-11-28 16:16:37 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script >
+import AppTitle from './components/AppTitle.vue';
+import AppMain from './components/AppMain.vue';
+
+export default{
+components:{AppTitle, AppMain}
+}
+</script>
+
+<template>
+  <AppTitle/>
+<AppMain/>
+
+
+</template>
+
+<style  lang="scss">
+@use"./style/general.scss";
+
+body{
+  background-color: orange;
+}
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,21 @@
+<script >
+export default{
+    
+}
+</script>
+
+<template>
+<section>
+    <h2 class="mx-5">Yu-Gi- Oh Api</h2>
+</section>   
+
+</template>
+
+<style lang="scss" scoped>
+section{
+   display: flex;
+   align-items: center;
+   background-color: white;
+
+}
+</style>
