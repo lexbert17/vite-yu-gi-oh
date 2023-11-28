de3636f252a35cdefefd29632e3ee220352ac6ef
lexbert17 <lexbertlazo@gmail.com>
2023-11-28 17:19:54 +0100

    add: creazione titolo e inserimento oggetto in store

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,31 @@
 <script >
 import AppTitle from './components/AppTitle.vue';
-import AppMain from './components/AppMain.vue';
-
+import AppCharactersList from './components/AppCharactersList.vue';
+import { store } from "./store";
+import axios from 'axios';
 export default{
-components:{AppTitle, AppMain}
+components:{ AppTitle, AppCharactersList },
+data() {
+  return{
+    store:store
+  }
+},
+methods: {
+    getCharacters(){ 
+        axios
+        .get("https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0")
+        .then(resp =>{
+            this.store.cardList = resp.data.data
+        });
+    }
+},
 }
+
 </script>
 
 <template>
-  <AppTitle/>
-<AppMain/>
+<AppTitle/>
+<AppCharactersList/>
 
 
 </template>
---
+++ src/components/AppCharactersList.vue
@@ -0,0 +1,19 @@
+<script>
+export default{
+
+}
+</script>
+
+<template>
+    <section class="container mt-5">
+        <h2>lista personaggi</h2>
+    </section>
+</template>
+
+<style lang="scss" scoped>
+section {
+color: green;
+background-color: white;
+}
+
+</style>
